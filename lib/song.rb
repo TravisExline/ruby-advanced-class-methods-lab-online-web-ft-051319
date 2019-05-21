5d49ac4f0bca6e3ba11a01a34362c97f030f28d3
Travis Exline <t.exline22@gmail.com>
2019-05-21 15:22:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,11 @@ class Song
   end
 
   def find_by_name(song_name)
-    self.all.each { |song_title| if song_title == song_name return song_title}
+    self.all.each do |song_title|
+      if song_title == song_name
+        return song_title
+      end
+    end
+    song_name
   end
 end
